3c561ad7bb80efa9088dfd1842780b1dfcc2f2b0
ashlee.williamson <ashlee.williamson@yorksj.ac.uk>
2024-03-27 23:24:14 +0000

    Can now load maps based on a string

--- map-gen.js
+++ map-gen.js
@@ -212,16 +212,129 @@ generateMap(); // Auto-generate map on page load
 
 
 // Function to generate map based on a tile string
-function generateMapFromTileString(tileString) {
-    // Parse the tile string into an array of tile numbers
-    const tileArray = tileString.split(', ').map(Number);
-    // Generate the map using the tile array
-    generateMap(tileArray);
-}
+function generateMapFromTileString() {
+    
+    const mapString = document.getElementById('mapString').value;
+    const tilesArray = mapString.split(',').map(number => parseInt(number.trim()));
 
-// Example usage:
-const tileString = '57, 75, 25, 37, 24, 21, 7, 29, 20, 8, 69, 28, 59, 32, 74, 65, 35, 34, 72, 36, 22, 71, 63, 62, 73, 33, 30, 56, 19, 27, 58, 76, 38, 31, 70, 61, 11';
-generateMapFromTileString(tileString);
+    const mapContainer = document.getElementById('map-container');
+    mapContainer.innerHTML = '';
+    
+    const tilePane = document.getElementById('tile-pane');
+    tilePane.innerHTML = ''; // Clear previous content
+    
+    // Coordinates for each hexagon tile
+    
+    //top: add 45px for each row
+    //left: add 160px for each tile
+    const tileCoordinates = [
+        { hs: "y", top: '1px', left: '240px' },   // Tile 1 HS
+
+        { top: '46px', left: '160px' },  // Tile 2
+        { top: '46px', left: '320px' },  // Tile 3
+    
+        { top: '93px', left: '80px' },   // Tile 4
+        { top: '93px', left: '240px' }, // Tile 5
+        { top: '93px', left: '400px' },  // Tile 6
+    
+        { hs: "y", top: '138px', left: '0px' },   // Tile 7 HS
+        { top: '138px', left: '160px' }, // Tile 8
+        { top: '138px', left: '320px' },  // Tile 9
+        { hs: "y", top: '138px', left: '480px' },   // Tile 10 HS
+    
+        { top: '183px', left: '80px' }, // Tile 11
+        { top: '183px', left: '240px' },  // Tile 12 
+        { top: '183px', left: '400px' },   // Tile 13
+            
+        { top: '228px', left: '0px' }, // Tile 14
+        { top: '228px', left: '160px' },  // Tile 15
+        { top: '228px', left: '320px' },   // Tile 16
+        { top: '228px', left: '480px' }, // Tile 17
+    
+        { top: '273px', left: '80px' },  // Tile 18
+        { top: '273px', left: '240px' },   // Tile 19 MECATOL
+        { top: '273px', left: '400px' }, // Tile 20
+    
+        { top: '318px', left: '0px' },  // Tile 21
+        { top: '318px', left: '160px' },   // Tile 22
+        { top: '318px', left: '320px' }, // Tile 23
+        { top: '318px', left: '480px' },  // Tile 24
+    
+        { top: '363px', left: '80px' },   // Tile 25
+        { top: '363px', left: '240px' }, // Tile 26
+        { top: '363px', left: '400px' },  // Tile 27
+            
+        { hs: "y", top: '408px', left: '0px' },   // Tile 28 HS 
+        { top: '408px', left: '160px' }, // Tile 29
+        { top: '408px', left: '320px' },  // Tile 30
+        { hs: "y", top: '408px', left: '480px' },   // Tile 31 HS
+    
+        { top: '453px', left: '80px' }, // Tile 32
+        { top: '453px', left: '240px' },  // Tile 33
+        { top: '453px', left: '400px' },   // Tile 34
+    
+        { top: '498px', left: '160px' }, // Tile 35
+        { top: '498px', left: '320px' },  // Tile 36
+    
+        { hs: "y", top: '543px', left: '240px' },   // Tile 37 HS
+        ];
+    
+    var usedTiles = [];
+    
+    for (let i = 0; i < tileCoordinates.length; i++) {
+        const tileNumber = i + 1;
+        const tile = document.createElement('div');
+        let availableTiles;
+            
+        availableTiles = tilesArray;
+                
+    
+        // Check if there are available tiles to place
+        if (availableTiles.length > 0) {
+                
+            const selectedTile = availableTiles[i];
+    
+            tile.style.backgroundImage = `url('/images/tiles/ST_${selectedTile}.png')`;
+            tile.dataset.tile = selectedTile;
+    
+            // Add the selected tile to the used tiles array
+            usedTiles.push(selectedTile);
+            } else {
+            // Handle case where no available tiles
+            console.log("No available tiles to place.");
+    
+            }
+    
+        if (tileNumber === 19) {
+            tile.style.backgroundImage = `url('/images/tiles/ST_18.png')`;
+            tile.dataset.tile = 18;
+            }
+    
+            // Set other styles
+        tile.style.top = tileCoordinates[i].top;
+        tile.style.left = tileCoordinates[i].left;
+        tile.classList.add('map-tile'); // Add the class here if needed
+    
+            // Set tile selection event listener
+        tile.addEventListener('click', () => {
+            tile.classList.toggle('selected'); // Toggle the 'selected' class
+        });
+    
+    
+        // Append the tile to the map container
+        mapContainer.appendChild(tile);
+    
+    
+    
+            // Create images for all tiles and append them to the tile pane
+    
+    }
+    
+    printUsedTiles(usedTiles);
+    selectTile(usedTiles); // Call function to handle tile selection on the map
+    generateTilePane();
+}
+    
 
 
 function generateTilePane(colour) {
