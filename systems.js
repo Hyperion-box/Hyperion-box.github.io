3ba2a3d14f1326a4d33bc7f409b06925aeb43b56
ashlee.williamson <ashlee.williamson@yorksj.ac.uk>
2024-06-04 21:11:28 +0100

    added twilight-wars-map

---
+++ systems.js
@@ -0,0 +1,55 @@
+export const systems = [
+    // North West
+    {
+      "name": "The Shaleri Passage",
+      "type": "planet",
+      "x": 10,
+      "y": 20,
+      "image": "path_to_image_A.png",
+      "label": ""
+    },
+    {
+      "name": "The Ghost Worlds",
+      "type": "delta-wormhole",
+      "x": 15,
+      "y": 5,
+      "image": "/images/tech-icon/cybernetic.png"
+    },
+    {
+      "name": "Kal Haddar ",
+      "type": "star",
+      "x": 10,
+      "y": 10,
+      "label": "N",
+      "image": "/images/tech-icon/cybernetic.png"
+    },
+
+    // North East
+
+    {
+        "name": "Gul System*",
+        "type": "mecatol-rex",
+        "x": 43,
+        "y": 16,
+        "label": "N",
+        "image": "/images/planets-no-bg/MR-Planet.png"
+      },
+
+    {
+        "name": "Saudor",
+        "type": "industrial-planet",
+        "x":38,
+        "y": 15,
+        "label": "N",
+        "image": "/images/tech-icon/cybernetic.png"
+      },
+
+      {
+        "name": "Lodor",
+        "type": "cultural-planet",
+        "x":39,
+        "y": 25,
+        "label": "N",
+        "image": "/images/tech-icon/cybernetic.png"
+      }
+  ]
