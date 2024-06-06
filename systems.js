b2cc535a48392df638b0251279efdc39b4393bb4
ashlee.williamson <ashlee.williamson@yorksj.ac.uk>
2024-06-06 19:07:40 +0100

    Added planet labels, fixed point coords, label div added

--- systems.js
+++ systems.js
@@ -5,15 +5,22 @@ export let systems = [
         "x": 24,
         "y": 30,
         "image": "path_to_image_A.png",
-        "info": ""
+        "info": "",
+        "planet-1": "",
+        "planet-2": "",
+        "planet-3": "",
+        "planet-4": "",
+        "planet-5": "",
+        "planet-6": "",
+        "planet-7": "",
       },
     
     // North West
     {
       "name": "The Shaleri Passage",
       "type": "planet",
-      "x": 18,
-      "y": 20,
+      "x": 16,
+      "y": 21,
       "image": "path_to_image_A.png",
       "info": ""
     },
@@ -38,17 +45,24 @@ export let systems = [
     {
         "name": "Gul System*",
         "type": "mecatol-rex",
-        "x": 43,
-        "y": 16,
-        "info": "System: Gul Planets: - Mecatol Rex \n - Jun \n - Semani",
-        "image": "/images/planets-no-bg/MR-Planet.png"
+        "x": 34,
+        "y": 15,
+        "info": "System: Gul Planets: - Mecatol Rex",
+        "image": "/images/planets-no-bg/MR-Planet.png",
+        "planet-1": "Mecatol Rex",
+        "planet-2": "Jun",
+        "planet-3": "Semani",
+        "planet-4": "",
+        "planet-5": "",
+        "planet-6": "",
+        "planet-7": "",
       },
 
     {
         "name": "Saudor",
         "type": "industrial-planet",
         "x":38,
-        "y": 15,
+        "y": 14,
         "info": "N",
         "image": "/images/tech-icon/cybernetic.png"
       },
@@ -57,7 +71,7 @@ export let systems = [
         "name": "Lodor",
         "type": "cultural-planet",
         "x":39,
-        "y": 25,
+        "y": 22,
         "info": "N",
         "image": "/images/tech-icon/cybernetic.png"
       },
@@ -66,9 +80,24 @@ export let systems = [
       {
         "name": "Quann",
         "type": "cultural-planet",
-        "x": 81,
-        "y": 45,
+        "x": 33,
+        "y": 36,
         "info": "N",
         "image": "/images/tech-icon/cybernetic.png"
+      },
+      {
+        "name": "Jorun System",
+        "type": "home-planet",
+        "x": 42,
+        "y": 34,
+        "info": "N",
+        "image": "/images/tech-icon/cybernetic.png",
+        "planet-1": "Ragh",
+        "planet-2": "Lisis II",
+        "planet-3": "",
+        "planet-4": "",
+        "planet-5": "",
+        "planet-6": "",
+        "planet-7": "",
       }
   ]
